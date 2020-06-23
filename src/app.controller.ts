74c9ec6e6df05a871e7987d318313d24a4212aaa
tsabunkar <tsabunkar@gmail.com>
2020-06-23 06:56:35 +0530

    Response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Req, Body } from '@nestjs/common';
+import { Controller, Get, Req, Body, Query, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -12,13 +12,33 @@ export class AppController {
   }
 
   @Get('cats')
-  findAllCats(@Req() request: Request, @Body() reqBody: Request): string {
+  findAllCats(@Req() request: Request): string {
     // Req - access the request object of Express
     console.log(request.body);
-    // it's not necessary to grab these properties manually.
-    // We can use dedicated decorators instead, such as @Body() or @Query()
-    // for complete list - [.assets/list-of-decorators]
-    console.log('Request bodys is - ', reqBody);
     return `Resp - ${JSON.stringify(request.body)}`;
+  }
+
+  /**
+   * it's not necessary to grab these properties manually.
+   * We can use dedicated decorators instead, such as @Body() or @Query()
+   * for complete list - [.assets/list-of-decorators]
+   */
+  @Get('cats1')
+  findAllCats1(@Body() reqBody: Request): string {
+    console.log('Request bodys is - ', reqBody);
+    return `Resp - ${JSON.stringify(reqBody)}`;
+  }
+
+  // http://localhost:3000/cats2?name=tejas&age=25
+  @Get('cats2')
+  findAllCats2(@Query() reqQuery: Request, @Res() response: Response): void {
+    console.log('Request Query is - ', reqQuery);
+    // return `Resp - ${JSON.stringify(reqQuery)}`;
+    /**
+     * Nest into Library-specific mode for that handler,  and you become responsible
+     * for managing the response.
+     * @Res or @Response()
+     */
+    response.send(reqQuery);
   }
 }
